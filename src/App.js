da205ea71e753226c256bfc3f1a02d8f63e1ec1e
Aditya Pratap <adityapratap7344@gmail.com>
2020-09-07 21:47:24 +0530

    added auth hook

--- src/App.js
+++ src/App.js
@@ -1,33 +1,49 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+import { useAuth } from './hooks/auth-hook';
 import { navigationRoutes } from './navigation/routes';
 import MainNavigation from './navigation/MainNavigation';
 import AuthScreen from './screens/AuthScreen';
+import SplashScreen from './screens/SplashScreen';
 
 function App() {
+  const { checkingAuthState, loggedIn, login, logout } = useAuth();
+
+  if (checkingAuthState) {
+    return <SplashScreen />;
+  }
 
   let routes;
-  routes = (
-    <Switch>
-      <Route exact path={navigationRoutes.HOME}>
-        <h1>HOME</h1>
-      </Route>
-      <Route exact path={navigationRoutes.AUTH}>
-        <AuthScreen />
-      </Route>
-      <Route exact path={navigationRoutes.USERS}>
-        <h1>USER LIST</h1>
-      </Route>
-      <Route exact path={navigationRoutes.NOTIFICATIONS}>
-        <h1>NOTIFICATIONS</h1>
-      </Route>
-      <Route exact path={navigationRoutes.INTERVIEW_EXPERIENCES}>
-        <h1>INTERVIEW_EXPERIENCES</h1>
-      </Route>
-      <Redirect to={navigationRoutes.HOME} />
-    </Switch>
-  );
+  if (loggedIn) {
+    routes = (
+      <Switch>
+        <Route exact path={navigationRoutes.HOME}>
+          <h1>HOME</h1>
+        </Route>
+        <Route exact path={navigationRoutes.USERS}>
+          <h1>USER LIST</h1>
+        </Route>
+        <Route exact path={navigationRoutes.NOTIFICATIONS}>
+          <h1>NOTIFICATIONS</h1>
+        </Route>
+        <Route exact path={navigationRoutes.INTERVIEW_EXPERIENCES}>
+          <h1>INTERVIEW_EXPERIENCES</h1>
+        </Route>
+        <Redirect to={navigationRoutes.HOME} />
+      </Switch>
+    );
+  } else {
+    routes = (
+      <Switch>
+        <Route exact path={navigationRoutes.AUTH}>
+          <AuthScreen />
+        </Route>
+        <Redirect to={navigationRoutes.AUTH} />
+      </Switch>
+    );
+  }
+
 
   return (
     <div className="App">
---
+++ src/hooks/auth-hook.js
@@ -0,0 +1,49 @@
+import { useState, useCallback, useEffect } from 'react';
+
+let logoutTimer;
+
+export const useAuth = () => {
+    const [checkingAuthState, setCheckingAuthState] = useState(true);
+    const [loggedIn, setLoggedIn] = useState(false);
+    const [expirationDate, setExpirationDate] = useState();
+
+    const login = useCallback((expiration = null) => {
+        setLoggedIn(true);
+
+        const tokenExpirationDate = expiration || new Date(new Date().getTime() + 1000 * 60 * 30);
+        setExpirationDate(tokenExpirationDate);
+
+        localStorage.setItem('userData', JSON.stringify({
+            expirationDate: tokenExpirationDate.toISOString(),
+        }));
+    }, []);
+
+    const logout = useCallback(() => {
+        setLoggedIn(false);
+        setExpirationDate(null);
+        localStorage.removeItem('userData');
+    }, []);
+
+    useEffect(() => {
+        if (expirationDate) {
+            const remainingTime = expirationDate.getTime() - new Date().getTime();
+            logoutTimer = setTimeout(logout, remainingTime);
+        } else {
+            clearTimeout(logoutTimer);
+        }
+    }, [expirationDate, logout]);
+
+    useEffect(() => {
+        const localUserData = localStorage.getItem('userData');
+        if (localUserData) {
+            const userData = JSON.parse(localUserData);
+            if (new Date(userData.expirationDate) > new Date()) {
+                login(userData.expirationDate);
+            }
+        }
+
+        setCheckingAuthState(false);
+    }, [login]);
+
+    return { checkingAuthState, loggedIn, login, logout };
+};
