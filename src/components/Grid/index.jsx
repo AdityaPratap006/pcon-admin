2a555f9588523b602cd0a8086e7dbc0888c19fc9
Aditya Pratap <adityapratap7344@gmail.com>
2020-09-07 23:35:16 +0530

    created grid component

---
+++ src/components/Grid/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './index.module.scss';
+
+const Grid = () => {
+    return (
+        <div className={styles['grid']}>
+
+        </div>
+    );
+};
+
+export default Grid;
