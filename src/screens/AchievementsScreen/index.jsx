111d1fd234be7ac8dc73f7c2ea4fd5bc3def9c1b
Aditya Pratap <adityapratap7344@gmail.com>
2020-09-11 20:26:06 +0530

    added achievements screen

--- src/screens/AchievementsScreen/index.jsx
+++ src/screens/AchievementsScreen/index.jsx
@@ -7,24 +7,67 @@ import Card from '../../components/Card';
 import LoadingSpinner from '../../components/LoadingSpinner';
 import { FaPlus } from 'react-icons/fa';
 import { navigationRoutes } from '../../navigation/routes';
+import { achievementsRef } from '../../firebase/firebase.utils';
+
+function compareAchievements(achievement1, achievement2) {
+
+    const date1 = new Date(`${achievement1.month} ${achievement1.year}`);
+    const date2 = new Date(`${achievement2.month} ${achievement2.year}`);
+
+    if (date1 > date2) {
+        return -1;
+    } else if (date1 < date2) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
 
 const AchievementsScreen = () => {
+    const [achievements, setAchievements] = useState([]);
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        const fetchAchievements = async () => {
-            await new Promise(res => setTimeout(res, 500));
-            setLoading(false);
+        let listener = null;
+
+        const fetchAchievementList = async () => {
+            listener = achievementsRef.on('value', listSnapshot => {
+
+                const achievementList = [];
+
+                listSnapshot.forEach(snapshot => {
+                    achievementList.push({
+                        id: snapshot.key,
+                        ...snapshot.val()
+                    });
+                });
+
+                achievementList.sort(compareAchievements);
+
+                setAchievements(achievementList);
+                setLoading(false);
+            });
         }
 
-        fetchAchievements();
+        fetchAchievementList();
+
+        return () => {
+            if (listener) {
+                achievementsRef.off('value', listener);
+            }
+        };
 
     }, []);
 
-    const renderedCards = [...Array(10).keys()].map(ach => {
+    const renderedCards = achievements.map(ach => {
         return (
-            <Card key={ach} className={styles['achievement-card']}>
-
+            <Card key={ach.id} className={styles['achievement-card']}>
+                <h3 className={styles['title']}>{ach.title}</h3>
+                <div className={styles['info']}>
+                    <span className={styles['date']}>{ach.month} {ach.year}</span>
+                    <span className={styles['location']}>{ach.location}</span>
+                </div>
+                <p className={styles['content']}>{ach.content}</p>
             </Card>
         );
     });
