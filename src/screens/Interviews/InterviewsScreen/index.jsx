a0a8c746d5d6387e1266ae27ab9ad413a60e3a6d
Aditya Pratap <adityapratap7344@gmail.com>
2020-10-03 20:25:51 +0530

    sorted interviews by date

--- src/screens/Interviews/InterviewsScreen/index.jsx
+++ src/screens/Interviews/InterviewsScreen/index.jsx
@@ -21,6 +21,21 @@ const InterviewsScreen = () => {
                 ...interviewDoc.data(),
             }));
 
+            interviewsList.sort((interview1, interview2) => {
+                const localDate1 = getLocalDateFromFirebaseTimestamp(interview1.createdAt);
+                const localDate2 = getLocalDateFromFirebaseTimestamp(interview2.createdAt);
+                const date1 = new Date(localDate1);
+                const date2 = new Date(localDate2);
+
+                if (date1 < date2) {
+                    return 1;
+                } else if (date1 > date2) {
+                    return -1;
+                } else {
+                    return 0;
+                }
+            });
+
             setInterviews(interviewsList);
             setLoading(false);
         });
